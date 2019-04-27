9325dfbbfaf383ba0bc175d9670f6280ddf6addf
mushut <mushut@gmail.com>
2019-04-27 03:51:52 +0300

    First files

---
+++ src/myshell.cpp
@@ -0,0 +1,21 @@
+/*
+ * myshell program
+ * Aki Sipovaara 2019
+ */
+
+#include <iostream>
+
+/*
+ * Program may be passed a script file with commands listed.
+ * Otherwise commands must be written on the shell itself
+ */
+int main(int argc, char **argv) {
+	std::cout << "myshell" << std::endl;
+
+	// program run with arguments
+	
+	// program run without arguments
+	// current path
+
+	return 0;
+}
