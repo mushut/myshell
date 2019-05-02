3256a850a668fd7997e19715913cece1c67b20d9
mushut <mushut@gmail.com>
2019-05-02 05:20:01 +0300

    More definition of path class

---
+++ src/path.cpp
@@ -0,0 +1,16 @@
+#include "path.h"
+
+Path::Path(void) {
+}
+
+Path::Path(std::string new_path) {
+}
+
+bool Path::change_path(std::string) {
+}
+
+std::vector<std::string> Path::parse_path(std::string) {
+}
+
+std::sring Path::to_string(void) {
+}
--- src/path.h
+++ src/path.h
@@ -4,6 +4,7 @@ public:
 	Path(std::string new_path);
 	bool change_path(std::string);
 	std::vector<std::string> parse_path(std::string);
+	std::string to_string(void);
 private:
 	std::string path_string;
 	std::vector<std::string> path;
