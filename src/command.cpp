201f0929cf7765d44d6c83a0a935f3a81dcfbb3e
mushut <mushut@gmail.com>
2019-05-05 08:47:29 +0300

    Progress

--- src/command.cpp
+++ src/command.cpp
@@ -1,5 +1,17 @@
 #include "command.h"
 
+#include <iostream>
+
+extern "C" {
+#include <sys/stat.h>
+}
+
+
+Command::Command(void) {
+	type = "Not initialized";
+	parameters = NULL;
+}
+
 Command::Command(std::string new_type, std::vector<std::string> new_parameters) {
 	// Constructor
 	type = new_type;
@@ -10,6 +22,14 @@ bool Command::execute(void) {
 	// Execute command
 	if (type == "list") {
 		// List command
+
+		// TESTING: test basic functions first
+		struct stat buffer;
+		int result = 0;
+
+		result = stat("/home/asipovaa/", &buffer);
+		std::cout << buffer.uid_t << std::endl;
+		std::cout << "Result: " << result << std::endl;
 
 		return true;
 	}
--- src/command.h
+++ src/command.h
@@ -1,10 +1,12 @@
 #include <vector>
+#include <string>
 
 class Command {
 public:
+	Command(void);
 	Command(std::string new_type, std::vector<std::string> new_parameters);
 	bool execute(void);
 private:
 	std::string type;
 	std::vector<std::string> parameters;
-}
+};
